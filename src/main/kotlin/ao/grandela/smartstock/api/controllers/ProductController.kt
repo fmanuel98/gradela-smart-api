6898857871c4fff152f2263c7aa906c6b5166da3
fmanuel98 <faugf320@gmail.com>
2021-10-16 01:08:59 +0100

    first

--- src/main/kotlin/ao/grandela/smartstock/api/controllers/ProductController.kt
+++ src/main/kotlin/ao/grandela/smartstock/api/controllers/ProductController.kt
@@ -1,9 +1,19 @@
 package ao.grandela.smartstock.api.controllers
 
+import ao.grandela.smartstock.domain.models.Product
+import ao.grandela.smartstock.domain.models.ProductRepository
+import javax.transaction.Transactional
 import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 
 @Path("products")
-class ProductController {
-  @GET fun listar() = "Quarkus"
+class ProductController(val repository: ProductRepository) {
+  @GET fun listar() = repository.listAll()
+
+  @POST
+  @Transactional
+  fun salvar(product: Product): Unit {
+    repository.persist(product)
+  }
 }
